e90a532ad4a0eff842aa4ba3ddcc46a9dfa2d534
Picoseconds <rw7@proacad.ca>
2020-08-05 16:00:40 -0600

    feat: added rudimentary API

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,15 @@ import http from 'http';
 import dotenv from 'dotenv';
 import express from 'express';
 import nunjucks from 'nunjucks';
+import SHA256 from 'fast-sha256';
+import arrayBufferToHex from 'array-buffer-to-hex';
 
 import * as console from './console';
 import { Server as WSServer } from 'ws';
 import UptimeWSServer from './uptimeWS';
 import { startServer } from './moomoo/moomoo';
+import { getGame } from './moomoo/Game';
+import { TextEncoder } from 'util';
 
 const app = express();
 const server = http.createServer(app);
@@ -16,40 +20,73 @@ const port = process.env.PORT;
 dotenv.config();
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+  autoescape: true,
+  express: app
 });
 
 const VERSION = "0.0.0a";
 
 function format(timestamp: number) {
-    var hours = Math.floor(timestamp / (60 * 60));
-    var minutes = Math.floor(timestamp % (60 * 60) / 60);
-    var seconds = Math.floor(timestamp % 60);
+  var hours = Math.floor(timestamp / (60 * 60));
+  var minutes = Math.floor(timestamp % (60 * 60) / 60);
+  var seconds = Math.floor(timestamp % 60);
 
-    return hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+  return hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
 }
 
 app.get('/sanctuary', (req, res) => {
-    if (req.accepts('html')) {
-        res.render('version.html', { version: VERSION, nodeVersion: process.version, uptime: format(process.uptime()) });
-        return;
-    }
+  if (req.accepts('html')) {
+    res.render('version.html', { version: VERSION, nodeVersion: process.version, uptime: format(process.uptime()) });
+    return;
+  }
 
-    res.send('Sanctuary v${VERSION}');
+  res.send('Sanctuary v${VERSION}');
 });
 
 app.get('/uptime', (req, res) => {
-    if (req.accepts('html')) {
-        res.redirect('/sanctuary');
-        return;
-    }
+  if (req.accepts('html')) {
+    res.redirect('/sanctuary');
+    return;
+  }
 
-    res.send(format(process.uptime()));
+  res.send(format(process.uptime()));
 });
 
 app.get('/', (req, res) => {
-    res.redirect(`${req.protocol}://moomoo.io`)
+  res.redirect(`${req.protocol}://moomoo.io`)
+});
+
+app.get('/api/v1/playerCount', (_req, res) => {
+
+  let game = getGame();
+
+  if (!game) {
+    res.send(JSON.stringify({ type: "error", message: "No game active." }));
+  } else {
+    res.send(JSON.stringify({ type: "success", playerCount: game.clients.length }));
+  }
+});
+
+app.get('/api/v1/players', (req, res) => {
+  let game = getGame();
+
+  if (!game) {
+    res.send(JSON.stringify({ type: "error", message: "No game active." }));
+  } else {
+    let clients: { clientIPHash: string, playerName: string, playerID: number }[] = [];
+
+    for (let client of game.clients) {
+      clients.push(
+        {
+          clientIPHash: arrayBufferToHex(SHA256(new TextEncoder().encode(client.ip))),
+          playerName: client.player?.name || "unknown",
+          playerID: client.player?.id || -1
+        }
+      );
+    }
+
+    res.send(JSON.stringify({ type: "success", data: JSON.stringify(clients) }));
+  }
 });
 
 let wss = new WSServer({ noServer: true });
